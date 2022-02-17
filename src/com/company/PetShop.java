d2c9c9369f42e8aeff76330836f9eef31ba31487
Amanda Vollmar <mandavollmar@Amandas-MBP.hsd1.co.comcast.net>
2022-02-17 11:23:05 -0700

    classes made

---
+++ src/com/company/PetShop.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class PetShop {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("We have the following animals available: ");
+    }
+}
