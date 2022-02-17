d800b7ac338ae4556ba7c36daffe75d97d39de70
Amanda Vollmar <mandavollmar@Amandas-MBP.hsd1.co.comcast.net>
2022-02-17 11:31:41 -0700

    Setters/getters

--- src/com/company/Cat.java
+++ src/com/company/Cat.java
@@ -4,4 +4,23 @@ public class Cat {
     private String name;
     private int age;
     private String color;
+
+    public void setName (String nameOfCat) {
+        name = nameOfCat;
+    }
+    public String getName (){
+        return name;
+    }
+    public void setAge (int ageOfCat) {
+        age = ageOfCat;
+    }
+    public int getAge (){
+        return age;
+    }
+    public void setColor (String colorOfCat) {
+        color = colorOfCat;
+    }
+    public String getColor (){
+        return color;
+    }
 }
--- src/com/company/Dog.java
+++ src/com/company/Dog.java
@@ -1,4 +1,26 @@
 package com.company;
 
 public class Dog {
+    private String name;
+    private int age;
+    private String color;
+
+    public void setName (String nameOfDog) {
+        name = nameOfDog;
+    }
+    public String getName (){
+        return name;
+    }
+    public void setAge (int ageOfDog) {
+        age = ageOfDog;
+    }
+    public int getAge (){
+        return age;
+    }
+    public void setColor (String colorOfDog) {
+        color = colorOfDog;
+    }
+    public String getColor (){
+        return color;
+    }
 }
